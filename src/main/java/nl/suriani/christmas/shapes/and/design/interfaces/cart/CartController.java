c686e64135a42884d67348f631986e3ff7e324f6
suriani <tristano.suriani@ordina.nl>
2024-12-06 13:45:25 +0100

    Cart controller implemented.

--- src/main/java/nl/suriani/christmas/shapes/and/design/interfaces/cart/CartController.java
+++ src/main/java/nl/suriani/christmas/shapes/and/design/interfaces/cart/CartController.java
@@ -1,15 +1,29 @@
 package nl.suriani.christmas.shapes.and.design.interfaces.cart;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("api/cart")
+@Slf4j
 public class CartController {
 
-    @GetMapping
-    String hello() {
-        return "Hello cart!";
+    @PostMapping("add")
+    void add(@RequestBody AddToCart addToCart) {
+        log.info("Add to cart: {}", addToCart);
     }
+
+    @PostMapping("changeAmount")
+    void changeAmount(@RequestBody ChangeAmount changeAmount) {
+        log.info("Change amount: {}", changeAmount);
+    }
+
+    @PostMapping("remove")
+    void changeAmount(@RequestBody RemoveFromCart removeFromCart) {
+        log.info("Remove from cart: {}", removeFromCart);
+    }
+
+    record AddToCart(String itemId, int amount) {}
+    record ChangeAmount(String itemId, int amount) {}
+    record RemoveFromCart(String itemId) {}
 }
